ab1c6f3950d6e4e88ad8e2605c2cc6a96b79fc25
KyleRessLiere <kressliere@gmail.com>
2023-08-11 13:31:08 -0400

    changed from using app router to page router and setup intial file structure

---
+++ client/share-note/src/components/Homepage/Homepage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import home from "./Homepage.module.css";
+function Homepage() {
+  return <div className={home.background}>Homepage</div>;
+}
+
+export default Homepage;
---
+++ client/share-note/src/pages/index.js
@@ -0,0 +1,9 @@
+import Homepage from "../components/Homepage/Homepage";
+
+export default function Home() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
